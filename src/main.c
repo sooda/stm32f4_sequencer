6f1a7c1b56ad72e8b271581ec8cecdf4c41b1982
Konsta 'sooda' Hölttä <ksooda@gmail.com>
2014-05-29 11:54:35 +0300

    audio buffer fill testing

--- src/main.c
+++ src/main.c
@@ -9,16 +9,31 @@
 // Private variables
 volatile uint32_t time_var1, time_var2;
 
-// Private function prototypes
 static void AudioCallback(void *context,int buffer);
 void Delay(volatile uint32_t nCount);
 void init();
 
-// External variables
-extern const char mp3_data[];
+volatile int nextbuf;
+volatile int buf_consumed;
+#define AUDIOBUFSIZE 8192
+static int16_t audio_buffer[2][AUDIOBUFSIZE];
 
-// Some macros
-#define BUTTON		(GPIOA->IDR & GPIO_Pin_0)
+#define BUTTON (GPIOA->IDR & GPIO_Pin_0)
+
+void fillbuf(int16_t* buf) {
+	static uint32_t time;
+
+	GPIO_SetBits(GPIOD, GPIO_Pin_14);
+	for (int i = 0; i < AUDIOBUFSIZE/2; i++) {
+		int16_t sample = 0x7fff * (2.0 * time * (1.0f / 44100.0) * 123.0);
+		//int16_t sample = 0x7fff * sinf(2.0 * 3.14159 * time * (1.0f / 44100.0) * 123.0);
+		time++;
+		buf[2*i] = sample;
+		buf[2*i+1] = sample;
+	}
+	GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+}
+
 
 int main(void) {
 	init();
@@ -27,7 +42,7 @@ int main(void) {
 	InitializeAudio(Audio44100HzSettings);
 	SetAudioVolume(0xCF);
 	PlayAudioWithCallback(AudioCallback, 0);
-
+	fillbuf(audio_buffer[0]);
 	for(;;) {
 		/*
 		 * Check if user button is pressed
@@ -50,6 +65,11 @@ int main(void) {
 				while(BUTTON){};
 			}
 		}
+		if (buf_consumed) {
+			int buf = nextbuf;
+			buf_consumed = 0;
+			fillbuf(audio_buffer[buf]);
+		}
 	}
 
 	return 0;
@@ -62,26 +82,30 @@ int main(void) {
  * provided to the audio driver.
  */
 static void AudioCallback(void *context, int buffer) {
-	static int16_t audio_buffer[2][4096];
-	static int time;
-
-
+#if 0
 	if (buffer) {
 		GPIO_SetBits(GPIOD, GPIO_Pin_13);
-		GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+		//GPIO_ResetBits(GPIOD, GPIO_Pin_14);
 	} else {
-		GPIO_SetBits(GPIOD, GPIO_Pin_14);
+		//GPIO_SetBits(GPIOD, GPIO_Pin_14);
 		GPIO_ResetBits(GPIOD, GPIO_Pin_13);
 	}
+#else
+	GPIO_SetBits(GPIOD, GPIO_Pin_13);
+#endif
 
-	int nsamples = 4096;
+#if 0
+	nextbuf = 1 - buffer;
+	buf_consumed = 1;
+#else
+	fillbuf(audio_buffer[buffer]);
+#endif
 
-	for (int i = 0; i < nsamples; i++) {
-		int16_t sample = time += 50;
-		audio_buffer[buffer][i] = sample;
-	}
-
-	ProvideAudioBuffer(audio_buffer[buffer], nsamples);
+	//fillbuf(audio_buffer[buffer]);
+	ProvideAudioBuffer(audio_buffer[buffer], AUDIOBUFSIZE);
+#if 1
+	GPIO_ResetBits(GPIOD, GPIO_Pin_13);
+#endif
 }
 
 void init() {
